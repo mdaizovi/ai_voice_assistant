67088fcb62b5c270aa420296ebd994c5f77e09dc
Mic <mdaizovi@gmail.com>
2023-07-02 20:19:36 +0200

    remove unnecessary db code

--- src/main.py
+++ src/main.py
@@ -1,37 +1,25 @@
 # Third-party imports
 import openai
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Request
 from decouple import config
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 # Internal imports
-from models import Conversation, SessionLocal
 from utils import send_message, logger, ogg2mp3
 
 openai.api_key = config("OPENAI_API_KEY")
-
 app = FastAPI()
-
-# Dependency
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
 
 
 @app.post("/message/whatsapp")
-async def reply(request: Request, db: Session = Depends(get_db)):
+async def reply(request: Request):
     # Await for the incoming webhook request to extract information
     # like phone number and the media URL of the voice note
     form_data = await request.form()
     whatsapp_number = form_data['From'].split("whatsapp:")[-1]
-    print(f"Chatting with this number: {whatsapp_number}")
+    body = form_data['Body']
+    print(f"body {body}")
 
-    print(f"form_data {form_data}")
-    #TODO get name, stupid to alwasy use same filename
     media_url = form_data['MediaUrl0']
     media_type = form_data['MediaContentType0']
     print(f"Media URL: {media_url}\nMedia Content type: {media_type}")
@@ -52,33 +40,7 @@ async def reply(request: Request, db: Session = Depends(get_db)):
             Now it's being sent to ChatGPT API to reply...
         """)
 
-    # Call the OpenAI API to generate text with ChatGPT
-    messages = [{"role": "user", "content": whisper_response.get("text")}]
-    messages.append({"role": "system", "content": "You're an English teacher who has taught 100s of students grammar, idioms, vocab, basic English information, and beyond basics."})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=200,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
-
-
-    # The generated text
-    chatgpt_response = response.choices[0].message.content
-    # Store the conversation in the database
-    try:
-        conversation = Conversation(
-            sender=whatsapp_number,
-            message=whisper_response.get("text"),
-            response=chatgpt_response
-            )
-        db.add(conversation)
-        db.commit()
-        logger.info(f"Conversation #{conversation.id} stored in database")
-    except SQLAlchemyError as e:
-        db.rollback()
-        logger.error(f"Error storing conversation in database: {e}")
-    send_message(whatsapp_number, chatgpt_response)
+    text = whisper_response.get("text")
+    chat_response = f"I think you said: {text}"
+    send_message(whatsapp_number, chat_response)
     return ""
--- src/utils.py
+++ src/utils.py
@@ -16,6 +16,11 @@ auth_token = config("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 twilio_number = config('TWILIO_NUMBER')
 
+full_path = os.path.realpath(__file__)
+this_file_dir = os.path.dirname(full_path)
+AUDIO_IN_DIR = os.path.join(this_file_dir, "data", "input")
+AUDIO_OUT_DIR = os.path.join(this_file_dir, "data", "output")
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -33,14 +38,22 @@ def send_message(to_number, body_text):
         logger.error(f"Error sending message to {to_number}: {e}")
 
 def ogg2mp3(audio_url):
+        media_filename = audio_url.split('/')[-1]
+        print(f"media_filename {media_filename}")
+
+        print(f"AUDIO_IN_DIR {AUDIO_IN_DIR }")
+        print(f"AUDIO_OUT_DIR {AUDIO_OUT_DIR }")
+
         # Get the response of the OGG file
         response = requests.get(audio_url)
         # Get the redirect URL result
         url = response.url # `url` value something like this: "https://s3-external-1.amazonaws.com/media.twiliocdn.com/<some-hash>/<some-other-hash>"
         # Download the OGG file
-        urllib.request.urlretrieve(url, "data/audio.ogg")
+        input_filename = f"{AUDIO_IN_DIR}/{media_filename}.ogg" 
+        urllib.request.urlretrieve(url, input_filename)
         # Load the OGG file
-        audio_file = AudioSegment.from_ogg("data/audio.ogg")
+        audio_file = AudioSegment.from_ogg(input_filename)
         # Export the file as MP3
-        audio_file.export("data/audio.mp3", format="mp3")
-        return os.path.join(os.getcwd(), "data/audio.mp3")
+        input_filename = f"{AUDIO_OUT_DIR}/{media_filename}.mp3" 
+        audio_file.export(input_filename, format="mp3")
+        return os.path.join(input_filename)
